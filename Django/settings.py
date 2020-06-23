613eccaaeba6dcda3f24148694ca226d710b852d
Jiyoon <kjiyoon1@gmail.com>
2020-06-23 19:21:06 +0900

    html changes

--- Django/settings.py
+++ Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tkod85!w)e=1007()i5snrkpf*@pbl^v&kho!j@$5%##l+%xrd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
--- tokenizer/views.py
+++ tokenizer/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def token_list(request):
+    return render(request, 'tokenizer/token_list.html', {})
