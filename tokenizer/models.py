0be7a8ed4a40d3940e8a5818c11341fbc4e6f5e2
Jiyoon <kjiyoon1@gmail.com>
2020-06-23 17:03:54 +0900

    My Django app, first commit

---
+++ tokenizer/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Tokenoutput(models.Model):
+    textoutput = models.TextField
+
+    def __str__(self):
+        return self.textoutput
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Applications\PyCharm 2019.3.4\Django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
